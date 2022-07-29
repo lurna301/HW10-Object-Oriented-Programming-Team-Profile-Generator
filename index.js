0187899c2c568cac6f20614436472ae2671ad427
Lauren A <laurenagraphicdesign@gmail.com>
2022-07-29 10:26:23 -0400

    Inquirer working. All prompts generate, html page created with responses

--- index.js
+++ index.js
@@ -5,10 +5,85 @@ const inquirer = require("inquirer");
 const render = require("./src/page-template")
 const fs = require("fs");
 
-const teamMembers = [];
+const teamMembers = {
+    manager: null,
+    engineers: [],
+    interns:[]
+};
 
+const idArray =[]
 
+function createManager(){
 
+    inquirer.prompt([
+
+//Prompt for Manager ID input
+        {
+            type: 'input',
+            name: 'managerName',
+            validate: (answer) => {
+                if (answer !== ''){
+                    return true;
+                }
+                return "Please enter at least one character";
+            },
+        },
+//Prompt for Manager ID input
+        {
+            type: 'input',
+            name: 'managerID',
+            message: "Please enter team manager Id.",
+            validate: (answer) => {
+                const pass = answer.match(/^[1-9]\d*$/)
+                if (pass){
+                    return true
+                }
+                return 'Please enter a positive number greater than 0'
+            },
+
+        },
+//Prompt for Manager email input
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "Please enter team manager's email.",
+            validate: (answer) => {
+                const pass = answer.match(/\S+@\S+\.\S+/);
+                if (pass){
+                    return true
+                }
+                return 'Please enter a valid email.'
+            },
+
+        },
+
+//Prompt for Manager office number input
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: "Please enter team manager's office number.",
+            validate: (answer) => {
+                const pass = answer.match(/^[1-9]\d*$/)
+                if (pass){
+                    return true
+                }
+                return 'Please enter a positive number greater than 0'
+            },
+
+        },
+    ]).then((answers) => {
+        const manager = new Manager(
+            answers.managerName,
+            answers.managerID,
+            answers.managerEmail,
+            answers.managerOfficeNumber
+        )
+        teamMembers.manager = manager;
+        idArray.push(answers.managerID);
+        buildTeam();
+
+    });
+};
 
 
 function buildTeam(){
@@ -21,4 +96,4 @@ function buildTeam(){
 
 }
 
-buildTeam();
+createManager();
--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,74 @@
-function render(teamMembers){
-    return "this will be html"
+function renderManager(manager){
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title">${manager.getRole()}</h3>
+    </div>
+    </div>
+
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getID}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</li>
+            <li class="list-group-item">Office Number: ${manager.getOffice()}</li>
+        </ul>
+    </div>
+    `;
+}   
+function renderEngineers(engineers){
+    return `engineer`;
 }
+function renderInterns(interns){
+    return `intern`;
+}
+
+function generateTeam(teamMembers){
+    return `
+        ${renderManager(teamMembers.manager)}
+        ${renderEngineers(teamMembers.engineers)}
+        ${renderInterns(teamMembers.interns)}
+    
+    `
+}
+
+function render(teamMembers){
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css" integrity="sha384-zIaWifL2YFF1qaDiAo0JFgsmasocJ/rqu7LKYH8CoBEXqGbb9eO+Xi3s6fQhgFWM" crossorigin="anonymous"></link>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+    
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                        ${generateTeam(teamMembers)}
+                </div>
+            </div>
+        </div>
+
+    </body>
+    </html> 
+    `
+
+    
+   
+};
 
 module.exports = render;
